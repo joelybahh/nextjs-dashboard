466fd99df8cb775c86d7bee1fec1c5380e6855ca
Joel Gabriel <joel@inspacexr.com>
2023-10-29 21:22:34 +1100

    feat(dashboard): updated the invoice editing page to dynamically generate metadata based on the invoice and customer details  [no-key]
    - Imported fetchCustomerById from '@/app/lib/data'
    - Added a new type 'Props' for parameters and search parameters
    - Created a new async function 'generateMetadata' that fetches the invoice and customer data based on the id, generates title and description for the page dynamically
    - Replaced static metadata with dynamic metadata generated by 'generateMetadata'

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,15 +1,41 @@
-import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
+import { fetchCustomerById, fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
 import { InvoiceForm } from '@/app/lib/definitions';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import Form from '@/app/ui/invoices/update-form';
 
 import { notFound } from 'next/navigation';
 
-import { Metadata } from "next";
+import { Metadata, ResolvingMetadata } from "next";
 
-export const metadata: Metadata = {
-    title: 'Edit Invoice',
-};
+type Props = {
+    params: { id: string }
+    searchParams: { [key: string]: string | string[] | undefined }
+  }
+
+export async function generateMetadata(
+    { params, searchParams }: Props,
+    parent: ResolvingMetadata
+): Promise<Metadata> {
+    const id = params.id;
+
+    const invoice = await fetchInvoiceById(id);
+
+
+    if (!invoice) {
+        return notFound();
+    }
+
+    const customer = await fetchCustomerById(invoice?.customer_id as string);
+
+    if (!customer) {
+        return notFound();
+    }
+
+    return {
+        title: `Edit ${customer.name}'s Invoice`,
+        description: `Edit ${customer.name}'s Invoice for \$${invoice.amount}`,
+    };
+}
 
 
 export default async function Page({ params }: { params: { id: string } }) {
